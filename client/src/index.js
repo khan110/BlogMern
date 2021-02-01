1561b470bd5ab50c37128398f3a98e2b57982fe9
Umair ali khan <khan110@users.noreply.github.com>
2021-02-01 19:54:34 +0500

    a

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Editable from './components/Tables/EditableTable';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
